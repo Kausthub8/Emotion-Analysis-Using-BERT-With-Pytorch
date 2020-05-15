a796275fd153506bacb14f5e95b42303d5dad857
Dude-1999 <kausthub1369@gmail.com>
2020-05-15 19:41:17 +0530

    Add files via upload

---
+++ dataencod.py
@@ -0,0 +1,36 @@
+from transformers import BertTokenizer
+from torch.utils.data import TensorDataset
+
+
+import torch
+
+from tqdm.notebook import tqdm
+encoded_data_train = tokenizer.batch_encode_plus(
+    df[df.data_type=='train'].text.values,
+    add_special_tokens=True,
+    return_attention_mask=True,
+    pad_to_max_length=True,
+    max_length=256,
+    return_tensors='pt'
+)
+
+
+encoded_data_val = tokenizer.batch_encode_plus(
+    df[df.data_type=='val'].text.values,
+    add_special_tokens=True,
+    return_attention_mask=True,
+    pad_to_max_length=True,
+    max_length=256,
+    return_tensors='pt'
+)
+
+
+
+input_ids_train = encoded_data_train['input_ids']
+attention_masks_train = encoded_data_train['attention_mask']
+labels_train = torch.tensor(df[df.data_type=='train'].label.values)
+
+
+input_ids_val = encoded_data_val['input_ids']
+attention_masks_val = encoded_data_val['attention_mask']
+labels_val = torch.tensor(df[df.data_type=='val'].label.values)
